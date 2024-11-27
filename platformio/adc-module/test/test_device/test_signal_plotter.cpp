522ab24e771f1f402205fa625f39998636a71283
ettore-galli <ettore.galli@gmail.com>
2024-11-27 22:38:55 +0100

    Visual test

--- platformio/adc-module/test/test_device/test_signal_plotter.cpp
+++ platformio/adc-module/test/test_device/test_signal_plotter.cpp
@@ -1,6 +1,8 @@
 
 #include <Arduino.h>
 #include <signal_plotter.h>
+#include <math.h>
+#include <WString.h>
 
 SignalPlotter *plotter;
 
@@ -14,11 +16,33 @@ void setup(void)
     s = 0;
 }
 
-#include <WString.h>
-
-void loop(void)
+void linear_signal()
 {
     int h = s / 4;
     plotter->pushValue(h);
+}
+void linear_signal_inv()
+{
+    int h = 32 - s / 4;
+    plotter->pushValue(h);
+}
+
+void sine_signal()
+{
+    int h = 16 + int(16 * sin(2 * M_PI * s / 132));
+    plotter->pushValue(h);
+}
+
+void cosine_signal()
+{
+    int h = 16 + int(16 * cos(2 * M_PI * s / 132));
+    plotter->pushValue(h);
+}
+
+void loop(void)
+{
+
+    sine_signal();
+
     s = (s + 1) % display.getWidth();
 }
