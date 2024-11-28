c2129dd5aabac8b9b559e5f0c1c16a526a69e938
ettore-galli <ettore.galli@gmail.com>
2024-11-28 22:00:23 +0100

    Histogram

--- platformio/adc-module/src/signal_plotter.h
+++ platformio/adc-module/src/signal_plotter.h
@@ -3,33 +3,47 @@
 
 #define SCREEN_WIDTH 128
 
+void displayValuesChunkAsLine(int16_t values[SCREEN_WIDTH])
+{
+    display.clearBuffer();
+
+    for (int16_t x = 0; x < SCREEN_WIDTH; ++x)
+    {
+        display.drawPixel(x, values[x]);
+    }
+    display.sendBuffer();
+}
+
+void displayValuesChunkAsHistogram(int16_t values[SCREEN_WIDTH])
+{
+    display.clearBuffer();
+    display.setDrawColor(1);
+    for (int16_t x = 0; x < SCREEN_WIDTH; ++x)
+    {
+        display.drawVLine(x, display.getDisplayHeight() - values[x], values[x]);
+    }
+    display.sendBuffer();
+}
+
 class SignalPlotter
 {
 public:
     int16_t _x;
     int16_t _values[SCREEN_WIDTH];
+    int (*func)(int, int);
+    void (*_screenDraw)(int16_t _values[]);
 
     SignalPlotter()
     {
         _x = 0;
+        _screenDraw = displayValuesChunkAsHistogram;
     };
-
-    void displayValuesChunk()
-    {
-        display.clearBuffer();
-
-        for (int16_t x = 0; x < SCREEN_WIDTH; ++x)
-        {
-            display.drawPixel(x, _values[x]);
-        }
-        display.sendBuffer();
-    }
 
     void pushValue(int16_t v)
     {
         if (_x == SCREEN_WIDTH)
         {
-            displayValuesChunk();
+            _screenDraw(_values);
             _x = 0;
         }
         _values[_x] = v;
